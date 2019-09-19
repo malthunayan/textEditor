cc1f42d8cd32d07e4e41557af6b72ee7c1a62d32
Mohammad Al Thunayan <malthunayan4@gmail.com>
2019-09-19 10:47:38 +0300

    Mohammad Al Thunayan

--- src/App.js
+++ src/App.js
@@ -8,13 +8,47 @@ const styles = {
 };
 
 class App extends Component {
+  state = {
+    fontWeight: "normal",
+    fontStyle: "normal",
+    textDecoration: "none",
+    color: "black"
+  };
+  selectTypographicalEmphasis = emphasis => {
+    if (emphasis === "bold") {
+      if (this.state.fontWeight === "bold") {
+        this.setState({ fontWeight: "normal" });
+      } else {
+        this.setState({ fontWeight: "bold" });
+      }
+    } else if (emphasis === "italic") {
+      if (this.state.fontStyle === "italic") {
+        this.setState({ fontStyle: "normal" });
+      } else {
+        this.setState({ fontStyle: "italic" });
+      }
+    } else if (emphasis === "underline") {
+      if (this.state.textDecoration === "underline") {
+        this.setState({ textDecoration: "none" });
+      } else {
+        this.setState({ textDecoration: "underline" });
+      }
+    }
+  };
+  selectFontColor = fontColor => {
+    this.setState({ color: fontColor });
+  };
   render() {
     let styleNames = ["bold", "italic", "underline"];
     let colors = ["yellow", "blue", "red", "black", "purple"];
 
     let stylingBoxes = styleNames.map(style => {
       return (
-        <button style={styles[style]} key={style}>
+        <button
+          style={styles[style]}
+          key={style}
+          onClick={() => this.selectTypographicalEmphasis(`${style}`)}
+        >
           {style}
         </button>
       );
@@ -25,6 +59,7 @@ class App extends Component {
         <button
           style={{ backgroundColor: color, height: 30, width: 30 }}
           key={color}
+          onClick={() => this.selectFontColor(`${color}`)}
         />
       );
     });
@@ -32,7 +67,7 @@ class App extends Component {
     return (
       <div className="App">
         <div className="my-3">{stylingBoxes}</div>
-        <textarea />
+        <textarea style={this.state} />
         <div className="my-3">{colorBoxes}</div>
       </div>
     );
